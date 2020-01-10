1ecd02fe9c285ac88d0bdbbe24cca7b41bf6de38
Johni <johnatanosterloh@gmail.com>
2020-01-10 14:57:09 -0300

    Operadores Aritméticos

---
+++ Atribuicao_Operadores_Aritmeticos.java
@@ -0,0 +1,28 @@
+package aula1;
+
+public class Atribuicao_Operadores_Aritmeticos {
+
+	public static void main(String[] args) {
+		
+		int soma = 4;
+		soma += 2;		// = soma+2
+		System.out.println(soma);
+
+		int sub = 6;
+		sub -= 2;		// = sub-2
+		System.out.println(sub);
+		
+		int mult = 2;
+		mult *= 8;		// = mult*8
+		System.out.println(mult);
+		
+		int div = 8;
+		div /= 2;		// = div/2
+		System.out.println(div);
+		
+		int resto = 5;
+		resto %= 2;		// = resto%2
+		System.out.println(resto);
+	}
+
+}
