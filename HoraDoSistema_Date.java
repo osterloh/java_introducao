fbe3ade717ce8ac1e71b9e222c4c6e41af51e060
Johni <johnatanosterloh@gmail.com>
2020-01-10 15:00:50 -0300

    Retornando a hora com a classe Date

---
+++ HoraDoSistema_Date.java
@@ -0,0 +1,15 @@
+package aula1;
+
+import java.util.Date;
+
+public class HoraDoSistema_Date {
+
+	public static void main(String[] args) {
+		
+		Date relogio = new Date();
+		
+		System.out.println(relogio.toString());
+
+	}
+
+}
