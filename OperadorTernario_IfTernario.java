fdc7fa9c5c13ca7411d978a2f9fbd574c8e86607
Johni <johnatanosterloh@gmail.com>
2020-01-10 15:02:48 -0300

    If ternário

---
+++ OperadorTernario_IfTernario.java
@@ -0,0 +1,16 @@
+package aula1;
+
+public class OperadorTernario_IfTernario {
+
+	public static void main(String[] args) {
+		
+		int n1, n2, r;
+		n1 = 4;
+		n2 = 8;
+		r = (n1>n2) ? 0 : 1;
+		
+		System.out.println(r);
+
+	}
+
+}
